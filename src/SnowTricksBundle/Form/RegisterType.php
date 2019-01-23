9b38ecfe390aa49dea50974e693da33eb1a76edd
fabien hollebeque <hollebeque.fabien@hotmail.com>
2019-01-23 19:53:32 +0100

    Ajout des contraintes pour l'inscription

--- src/SnowTricksBundle/Form/RegisterType.php
+++ src/SnowTricksBundle/Form/RegisterType.php
@@ -8,6 +8,8 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegisterType extends AbstractType
 {
@@ -16,9 +18,12 @@ class RegisterType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('username', TextType::class, ['label' => 'Votre Pseudo :', 'required' => true])
-            ->add('plainPassword', PasswordType::class, ['label' => 'Votre Mot de passe :', 'required' => true])
-            ->add('email', EmailType::class, ['label' => 'Votre Email :', 'required' => true]);
+        $builder->add('username', TextType::class, ['label' => 'Votre Pseudo :', 'required' => true,
+                'constraints' => [new NotBlank(['message' => 'Veuillez renseigné votre pseudo'])]])
+            ->add('plainPassword', PasswordType::class, ['label' => 'Votre Mot de passe :', 'required' => true,
+                'constraints' => [new NotBlank(['message' => 'Veuillez renseigné votre mot de passe'])]])
+            ->add('email', EmailType::class, ['label' => 'Votre Email :', 'required' => true,
+                'constraints' => [new NotBlank(['message' => 'Veuillez renseigné votre email']), new Email(['message' => "Votre email {{ value }}, n'est pas un email valide"])]]);
     }
 
     /**
