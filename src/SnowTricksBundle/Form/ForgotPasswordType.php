4e0991f302abe03d96bf8f436d910ee2b49a3c0d
fabien hollebeque <hollebeque.fabien@hotmail.com>
2019-01-24 19:32:54 +0100

    Ajout et affichage du formulaire du mot de passe oublié

---
+++ src/SnowTricksBundle/Form/ForgotPasswordType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace SnowTricksBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ForgotPasswordType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('email', EmailType::class, ['label' => 'Votre Email :',
+            'constraints' => [new Email(['message' => "Votre email {{ value }} n'est pas un email valide"]),
+                              new NotBlank(['message' => "Veuillez complété le champ Email"])]]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'snowtricksbundle_user';
+    }
+}
