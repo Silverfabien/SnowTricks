8b286d72fb4c5cbce642e87322bc145317ce5b6a
fabien hollebeque <hollebeque.fabien@hotmail.com>
2019-01-24 20:11:08 +0100

    Ajout de l'entité Tricks

---
+++ src/SnowTricksBundle/Entity/Tricks.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace SnowTricksBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Tricks
+ *
+ * @ORM\Table(name="tricks")
+ * @ORM\Entity(repositoryClass="SnowTricksBundle\Repository\TricksRepository")
+ */
+class Tricks
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="figureGroup", type="string", length=255)
+     */
+    private $figureGroup;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="slug", type="string", length=255)
+     */
+    private $slug;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Tricks
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Tricks
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set figureGroup
+     *
+     * @param string $figureGroup
+     *
+     * @return Tricks
+     */
+    public function setFigureGroup($figureGroup)
+    {
+        $this->figureGroup = $figureGroup;
+
+        return $this;
+    }
+
+    /**
+     * Get figureGroup
+     *
+     * @return string
+     */
+    public function getFigureGroup()
+    {
+        return $this->figureGroup;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Tricks
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     *
+     * @return Tricks
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+}
+
