42a797fd5361c93fbc36e1a2824516f758b37ff1
fabien hollebeque <hollebeque.fabien@hotmail.com>
2019-01-25 17:06:42 +0100

    Ajout d'un tricks, affichages de tous les tricks et affichage d'un tricks en détails

--- src/SnowTricksBundle/Controller/TricksController.php
+++ src/SnowTricksBundle/Controller/TricksController.php
@@ -2,8 +2,12 @@
 
 namespace SnowTricksBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use SnowTricksBundle\Entity\Tricks;
+use SnowTricksBundle\Form\TricksType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class TricksController extends Controller
 {
@@ -12,15 +16,20 @@ class TricksController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('@SnowTricks/Default/index.html.twig');
+        $tricks = $this->getDoctrine()->getManager()->getRepository(Tricks::class);
+        $trick = $tricks->findAll();
+
+        return $this->render('@SnowTricks/Default/index.html.twig', ['tricks' => $trick]);
     }
 
     /**
      * @Route("/tricks/{slug}", name="snowtricks_viewtricks")
      */
-    public function viewAction()
+    public function viewAction($slug)
     {
+        $trick = $this->getDoctrine()->getRepository(Tricks::class)->findOneBy(['slug' => $slug]);
 
+        return $this->render('@SnowTricks/tricks/view.html.twig', ['trick' => $trick]);
     }
 
     /** Gestion des Tricks */
@@ -28,9 +37,22 @@ class TricksController extends Controller
     /**
      * @Route("/add/tricks", name="snowtricks_addtricks")
      */
-    public function addAction()
+    public function addAction(Request $request)
     {
+        $tricks = new Tricks();
+        $form = $this->createForm(TricksType::class, $tricks);
+        $form->handleRequest($request);
 
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($tricks);
+            $em->flush();
+
+            return $this->redirectToRoute('snowtricks_homepage');
+        }
+
+        return $this->render('@SnowTricks/tricks/add.html.twig', ['addTricksForm' => $form->createView()]);
     }
 
     /**
