5b84d79b4bfe3a13de0f5aeaada7c75549c3f079
fabien hollebeque <hollebeque.fabien@hotmail.com>
2019-01-27 19:37:21 +0100

    Ajout du listener pour l'upload ou le remove

---
+++ src/SnowTricksBundle/Listener/PictureListener.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SnowTricksBundle\Listener;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use SnowTricksBundle\Entity\TricksPicture;
+use SnowTricksBundle\Uploader\PictureUploader;
+
+class PictureListener
+{
+    private $pictureUploader;
+
+    public function __construct(PictureUploader $pictureUploader)
+    {
+        $this->pictureUploader = $pictureUploader;
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        if(!$entity instanceof TricksPicture)
+        {
+            return;
+        }
+
+        $fileName = $this->pictureUploader->upload($entity);
+
+        if($fileName === null)
+        {
+            return;
+        }
+
+        $entity->setFileName($fileName);
+    }
+
+    public function postRemove(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        if(!$entity instanceof TricksPicture)
+        {
+            return;
+        }
+
+        $this->pictureUploader->remove($entity);
+    }
+}
