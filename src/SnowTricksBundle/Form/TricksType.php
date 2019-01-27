82ae17419078aaf77c38b33c3348689526d6c11f
fabien hollebeque <hollebeque.fabien@hotmail.com>
2019-01-27 19:11:35 +0100

    Ajout des formulaires TricksPicture et TricksVideo graçe au collectionType

--- src/SnowTricksBundle/Form/TricksType.php
+++ src/SnowTricksBundle/Form/TricksType.php
@@ -3,6 +3,7 @@
 namespace SnowTricksBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -25,7 +26,11 @@ class TricksType extends AbstractType
                         new Length(['max' => 2000, 'maxMessage' => 'Le message ne doit pas dépasser 2000 caractères'])]])
                 ->add('figureGroup', TextType::class, ['label' => 'Groupe', 'attr' => ['placeholder' => 'Groupe du tricks'],
                     'constraints' => [new NotBlank(['message' => 'Veuillez remplir le champs groupe']),
-                        new Length(['max' => 255, 'maxMessage' => 'Le message ne doit pas dépasser 255 caractères'])]]);
+                        new Length(['max' => 255, 'maxMessage' => 'Le message ne doit pas dépasser 255 caractères'])]])
+                ->add('pictures', CollectionType::class, ['entry_type' => TricksPictureType::class,
+                    'entry_options' => ['label' => false], 'allow_add' => true, 'allow_delete' => true, 'by_reference' => false])
+                ->add('videos', CollectionType::class, ['entry_type' => TricksVideoType::class,
+                    'entry_options' => ['label' => false], 'allow_add' => true, 'allow_delete' => true, 'by_reference' => false]);
     }
 
     /**
